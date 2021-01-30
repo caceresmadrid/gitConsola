a37d65612af3c6c047b8166a990a786fb4a8a631
caceresmadrid <caceresmadrid@hotmail.com>
2021-01-29 18:57:58 -0600

    Agregamos el texto Este dia

--- GitConsola/Module1.vb
+++ GitConsola/Module1.vb
@@ -1,8 +1,11 @@
 ﻿Module Module1
 
     Sub Main()
+
         Console.WriteLine("Bienvenido")
         Console.WriteLine("a clases de Git")
+        Console.WriteLine("Este dia ")
+
 
 
     End Sub
